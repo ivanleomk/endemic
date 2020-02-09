c1f89af115ff95f67062eebf7baff535cdfef0b8
Reuben <reuben@strongarmtech.com>
2020-02-08 21:24:02 -0500

    updated

--- public/js/TestModel.js
+++ public/js/TestModel.js
@@ -1,11 +1,22 @@
 class Test{
-    constructor(long, lat, status, email, phoneNo, dateTime){
+    constructor(testId, ageGroup, long, lat, status, email, phoneNo, dateTime, 
+                isPregnant, isAsthma, isDiabetes, isLiver, isImmune, isBlood, isImmuneSystem, subjectFeeling){
+        this.testId = testId;
+        this.ageGroup = ageGroup;
         this.long = long;
         this.lat = lat;
         this.status = status;
         this.email = email;
         this.phoneNo = phoneNo;
         this.dateTime = dateTime;
+        this.isPregnant = isPregnant;
+        this.isAsthma = isAsthma;
+        this.isDiabetes = isDiabetes;
+        this.isLiver = isLiver;
+        this.isImmune = isImmune;
+        this.isBlood = isBlood;
+        this.isImmuneSystem = isImmuneSystem;
+        this.subjectFeeling = subjectFeeling;
     }
 
     toString() {
@@ -14,12 +25,22 @@ class Test{
 
     toFirestore(){
         return {
+            testId: this.testId,
+            ageGroup: this.ageGroup,
             long: this.long,
             lat: this.lat,
             status: this.status,
             email: this.email,
             phoneNo: this.phoneNo,
-            dateTime: this.dateTime
+            dateTime: this.dateTime,
+            isPregnant: this.isPregnant,
+            isAsthma: this.isAsthma,
+            isDiabetes: this.isDiabetes,
+            isLiver: this.isLiver,
+            isImmune: this.isImmune,
+            isBlood: this.isBlood,
+            isImmuneSystem: this.isImmuneSystem,
+            subjectFeeling: this.subjectFeeling
         }
     }
 }
